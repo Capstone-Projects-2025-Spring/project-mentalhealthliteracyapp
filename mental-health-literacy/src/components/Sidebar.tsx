6e0c6892893107ece07d387c8b7ff3689328d314
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-06-12 17:27:04 -0400

    Added ability to hide and show sidebar on mobile

--- mental-health-literacy/src/components/Sidebar.tsx
+++ mental-health-literacy/src/components/Sidebar.tsx
@@ -1,28 +1,51 @@
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar() {
+  const [sidebarStatus, setSidebarStatus] = useState<boolean>(true);
   return (
-    <div id="sidebar">
-      <img src={logo}></img>
-      <ul>
-        <li className={useLocation().pathname == "/" ? "active" : "inactive"}>
-          <Link to="/">Welcome</Link>
-        </li>
-        <li
-          className={
-            useLocation().pathname == "/tutorial" ? "active" : "inactive"
-          }
+    <>
+      <div id="menu-toggle">
+        <button
+          type="button"
+          className="resize-button"
+          onClick={() => setSidebarStatus(!sidebarStatus)}
         >
-          <Link to="/tutorial">Tutorial</Link>
-        </li>
-        <li
-          className={useLocation().pathname == "/video" ? "active" : "inactive"}
-        >
-          <Link to="/video">Video</Link>
-        </li>
-      </ul>
-    </div>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+
+      <aside id="sidebar" className={sidebarStatus ? "hidden" : "shown"}>
+        <nav>
+          <img src={logo}></img>
+
+          <ul>
+            <li
+              className={useLocation().pathname == "/" ? "active" : "inactive"}
+            >
+              <Link to="/">Welcome</Link>
+            </li>
+            <li
+              className={
+                useLocation().pathname == "/tutorial" ? "active" : "inactive"
+              }
+            >
+              <Link to="/tutorial">Tutorial</Link>
+            </li>
+            <li
+              className={
+                useLocation().pathname == "/video" ? "active" : "inactive"
+              }
+            >
+              <Link to="/video">Video</Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
   );
 }
 
