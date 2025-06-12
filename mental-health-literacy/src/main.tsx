51464d3d3c96ae065363366832d25aa0b4d6aaa7
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-06-12 17:27:53 -0400

    Changed layout to exclude topbar

--- mental-health-literacy/src/main.tsx
+++ mental-health-literacy/src/main.tsx
@@ -1,10 +1,9 @@
+import "./main.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./main.css";
 import Sidebar from "./components/Sidebar";
 import Welcome from "./pages/Welcome";
-import Topbar from "./components/Topbar";
 import Login from "./pages/Login";
 import Tutorial from "./pages/Tutorial";
 import Video from "./pages/Video";
@@ -13,8 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
       <Sidebar />
-      <main id="container">
-        <Topbar />
+      <main>
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
