29d3b7f3ffe4046558c5f55d7d0b85863c918694
kathrynalvarez <tui78504@temple.edu>
2025-06-17 03:50:45 -0400

    Basic login UI

--- mental-health-literacy/src/pages/Login.tsx
+++ mental-health-literacy/src/pages/Login.tsx
@@ -1,9 +1,21 @@
 function Login() {
-  return (
-    <div className="main-screen">
-      <h1>Login</h1>
-    </div>
-  );
+    return (
+        <div className="login-form">
+            <h1>Login</h1>
+            <form>
+                <label>Email:</label>
+                <input type="email" placeholder="Enter your email" required />
+
+                <label>Password:</label>
+                <input type="password" placeholder="Enter your password" required />
+
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
+
+
+
