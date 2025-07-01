58edc980cb2627efa56d7199e787ed09559ffaab
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-06-30 23:09:56 -0400

    Implemented dialog box for signin and register user actions

--- mental-health-literacy/src/components/Sidebar.tsx
+++ mental-health-literacy/src/components/Sidebar.tsx
@@ -1,14 +1,65 @@
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./Sidebar.css";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function Sidebar() {
   const [sidebarStatus, setSidebarStatus] = useState<boolean>(true);
+
+  const loginRef = useRef<HTMLDialogElement>(null);
+  const registerRef = useRef<HTMLDialogElement>(null);
+
+  const Close = function (type: string) {
+    switch (type) {
+      case "Login":
+        loginRef.current?.close();
+        break;
+      case "Signup":
+        registerRef.current?.close();
+        break;
+    }
+  };
+
+  const SwitchTo = function (type: string) {
+    Close(type);
+    switch (type) {
+      case "Login":
+        registerRef.current?.close();
+        loginRef.current?.showModal();
+        break;
+      case "Signup":
+        loginRef.current?.close();
+        registerRef.current?.showModal();
+        break;
+    }
+  };
+
   return (
     <>
+      <dialog ref={loginRef} className="dialog dialog-centered">
+        <Login
+          close={() => {
+            Close("Login");
+          }}
+          switch={() => SwitchTo("Signup")}
+        />
+      </dialog>
+
+      <dialog ref={registerRef} className="dialog dialog-centered">
+        <SignUp
+          close={() => {
+            Close("Signup");
+          }}
+          switch={() => {
+            SwitchTo("Login");
+          }}
+        />
+      </dialog>
+
       <div id="menu-toggle">
         <button
           type="button"
@@ -53,12 +104,28 @@ function Sidebar() {
           </ul>
         </nav>
         <div id="user-actions">
-          <Link to="/login" className="signin-btn">
+          {/* <Link to="/login" className="signin-btn">
             Sign-in
           </Link>
           <Link to="/signup" className="register-btn">
             Register
-          </Link>
+          </Link> */}
+          <button
+            className="signin-btn"
+            onClick={() => {
+              loginRef.current?.showModal();
+            }}
+          >
+            Sign-in
+          </button>
+          <button
+            className="register-btn"
+            onClick={() => {
+              registerRef.current?.showModal();
+            }}
+          >
+            Register
+          </button>
         </div>
       </aside>
     </>
--- mental-health-literacy/src/pages/Pages.tsx
+++ mental-health-literacy/src/pages/Pages.tsx
@@ -1,7 +1,5 @@
 import type { JSX } from "react";
 import Welcome from "./Welcome";
-import Login from "./Login";
-import SignUp from "./SignUp"
 import Tutorial from "./Tutorial";
 import Video from "./Video";
 import Resources from "./Resources";
@@ -13,11 +11,9 @@ export interface RouteDef {
 
 export const routeList: Array<RouteDef> = [
   { path: "/", element: <Welcome /> },
-  { path: "/login", element: <Login /> },
-  { path: "/signup", element: <SignUp /> },
   { path: "/tutorial", element: <Tutorial /> },
   { path: "/video", element: <Video /> },
   { path: "/resources", element: <Resources /> },
 ];
 
-export const noSidebarList = ["/login", "/signup"];
+export const noSidebarList = [];
