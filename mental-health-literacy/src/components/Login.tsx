c0bd4c222d8380532e2674ba8d612e912177d55e
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-07-16 11:34:39 -0400

    Converted forms to React Router Form

--- mental-health-literacy/src/components/Login.tsx
+++ mental-health-literacy/src/components/Login.tsx
@@ -1,25 +1,37 @@
 import "./Login.css";
 import CloseButton from "./CloseButton";
+import { Form, useFetcher } from "react-router-dom";
 function Login(props: any) {
+  const fetcher = useFetcher();
   return (
     <>
       <CloseButton close={props.close}></CloseButton>
 
       <div className="login-form">
         <h1>Login</h1>
-        <form>
+        <fetcher.Form method="post" action="/api/login">
           <label>
             Email:
-            <input type="email" placeholder="Enter your email" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              required
+            />
           </label>
 
           <label>
             Password:
-            <input type="password" placeholder="Enter your password" required />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              required
+            />
           </label>
 
           <button type="submit">Log In</button>
-        </form>
+        </fetcher.Form>
 
         <p style={{ marginTop: "1rem", textAlign: "center" }}>
           Don’t have an account?{" "}
--- mental-health-literacy/src/components/SignUp.tsx
+++ mental-health-literacy/src/components/SignUp.tsx
@@ -1,23 +1,40 @@
+import { useFetcher } from "react-router-dom";
 import CloseButton from "../components/CloseButton";
 
 function SignUp(props: any) {
+  const fetcher = useFetcher();
   return (
     <>
       <CloseButton close={props.close}></CloseButton>
       <div className="login-form">
         <h1>Sign Up</h1>
-        <form>
+        <fetcher.Form method="post" action="/api/register">
           <label>Email:</label>
-          <input type="email" placeholder="Enter your email" required />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+          />
 
           <label>Password:</label>
-          <input type="password" placeholder="Create a password" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Create a password"
+            required
+          />
 
           <label>Display Name:</label>
-          <input type="text" placeholder="Enter your name" required />
+          <input
+            type="text"
+            name="displayName"
+            placeholder="Enter your name"
+            required
+          />
 
           <button type="submit">Create Account</button>
-        </form>
+        </fetcher.Form>
 
         <p style={{ marginTop: "1rem", textAlign: "center" }}>
           Already have an account?{" "}
