b09ec2843a60142d331561637bb25b293d333507
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-07-19 22:28:08 -0400

    Disabled SSR since it is not utilized properly

--- mental-health-literacy/react-router.config.ts
+++ mental-health-literacy/react-router.config.ts
@@ -1,10 +1,9 @@
 import { type Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
+  ssr: false,
   future: {
     unstable_viteEnvironmentApi: true,
   },
   appDirectory: "src",
-  prerender: true,
 } satisfies Config;
